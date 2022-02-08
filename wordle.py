6aea9cf5386c0e0b877129f890a4db5e073b1883
Kody <94947670+KodyyJayy@users.noreply.github.com>
2022-02-08 15:28:39 +0000

    Update wordle.py

--- wordle.py
+++ wordle.py
@@ -18,7 +18,7 @@ def check_letters(answer_arr, guess_arr):
 	current_index = 0
 
 	for letter in guess_arr:
-		if guess_arr[current_index] == answer_arr[current_index]: #letter was in correct place
+		if guess_arr[current_index] == answer_arr[current_index]: # letter was in correct place
 			print(letter + ' +')
 
 		elif letter not in answer_arr: # letter is not in word
@@ -41,18 +41,18 @@ def check_letters(answer_arr, guess_arr):
 				if letter_place_arr.count(letter) >= freq_ans: # if the current letter is in the array more than the answer
 					print(letter + ' -')
 				else:
-					print(letter + ' /')
+					print(letter + ' /') 
 					letter_place_arr.append(letter)
 
 		current_index += 1
 	letter_place_arr.clear()
 
 def rematch():
-	answer = input('Play again? (y/n): ')
+	answer = input('\nPlay again? (y/n): ')
 
-	if answer.lower() == 'y':
+	if answer.lower().strip() == 'y':
 		start_game()
-	elif answer.lower() == 'n':
+	elif answer.lower().strip() == 'n':
 		clear_console()
 		exit()
 	else:
@@ -72,7 +72,7 @@ def start_game():
 	print(' • A slash (/) means the letter is in the word but not the correct place')
 	print('------------------------------------------------------------------------')
 	random_word = get_random_word('words.txt')
-	answer_arr = split_word(random_word.lower())
+	answer_arr = split_word(random_word.lower().strip())
 
 	max_rounds = 6
 	curr_round = 0
@@ -87,38 +87,38 @@ def start_game():
 		print('')
 		guess = input("Make a guess: ")
 
-		if guess.lower() == random_word.lower():
+		if guess.lower().strip() == random_word.lower().strip():
 			clear_console()
 			print('Correct!')
 			print('You got the word in ' + str(curr_round + 1) + (' tries!' if (curr_round + 1) > 1 else " try!"))
-			guess_arr = split_word(guess.lower())
+			guess_arr = split_word(guess.lower().strip())
 			check_letters(answer_arr, guess_arr)
 			rematch()
 
-		if len(guess) != 5:
+		if len(guess.strip()) != 5:
 			print('Guess must be 5 characters!')
 			continue
 
-		if not guess.isalpha():
+		if not (guess.strip()).isalpha():
 			print('Guess must only contain letters!')
 			continue
 
-		if not check_dict(guess.lower()):
+		if not check_dict(guess.lower().strip()):
 			print('Word not found in dictionary!')
 			continue
 
-		if guess in guessed_words:
+		if guess.lower().strip() in guessed_words:
 			print('You already tried this word!')
 			continue
 
-		if guess != random_word:
+		if guess.lower().strip() != random_word.lower().strip():
 			curr_round += 1
 			print("Incorrect!")
 			print("Guesses Remaining: " + str(max_rounds - curr_round))
 
-			guessed_words.append(guess.lower())
+			guessed_words.append(guess.lower().strip())
 
-			guess_arr = split_word(guess.lower())
+			guess_arr = split_word(guess.lower().strip())
 
 			check_letters(answer_arr, guess_arr)
 
